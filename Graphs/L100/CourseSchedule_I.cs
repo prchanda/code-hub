5cff897188523b74504cb9fc2483f12b29839675
Pratyaydeep Chanda <prchanda@microsoft.com>
2022-03-10 16:01:46 +0530

    feat(graph level 100): corrected code

--- Graphs/L100/CourseSchedule_I.cs
+++ Graphs/L100/CourseSchedule_I.cs
@@ -23,79 +23,80 @@ namespace CodeHub.Graphs.L100
     1. The main idea is to detect if there is a circular dependency bewteen the courses, if thats true then it's not possible to take
        all the courses.
 
-    2. To detect a cycle in a directed graph here we have implemented Disjoint data structure with Union and Find operation.
+    2. To detect a cycle in a directed graph using DFS.
 */
 
     class CourseSchedule_I<T>
     {
-        List<AdvancedVertex<T>> courses;
+        AdjacencyList<T> courses;
         Dictionary<T, int> indexLookup;
         T[,] courseDependencies;
         
         public CourseSchedule_I(T[,] dependencies)
         {
             courseDependencies = dependencies;
-            courses = new List<AdvancedVertex<T>>(dependencies.GetLength(0));
+            courses = new AdjacencyList<T>(true);
             indexLookup = new Dictionary<T, int>();
             for (int row = 0; row < dependencies.GetLength(0); row++)
             {
                 if(!indexLookup.ContainsKey(dependencies[row, 0]))
                 {
-                    courses.Add(new AdvancedVertex<T>(dependencies[row, 0], dependencies[row,0]));
-                    indexLookup.Add(dependencies[row, 0], courses.Count - 1);
+                    courses.AddVertex(dependencies[row, 0]);                    
+                    indexLookup.Add(dependencies[row, 0], courses.Vertices.Count - 1);
                 }
                 if(!indexLookup.ContainsKey(dependencies[row, 1]))
                 {
-                    courses.Add(new AdvancedVertex<T>(dependencies[row, 1], dependencies[row,1]));
-                    indexLookup.Add(dependencies[row, 1], courses.Count - 1);
+                    courses.AddVertex(dependencies[row, 1]);                    
+                    indexLookup.Add(dependencies[row, 1], courses.Vertices.Count - 1);
                 }
-            }
-        }
-
-        private T GetParent(T child)
-        {
-            int childIndex = indexLookup[child];
-            return courses[childIndex].Parent;
-        }
-
-        private T Find(T child)
-        {
-            T parent = GetParent(child);
-            AdvancedVertex<T> childVertex = courses[indexLookup[child]];
-            if(!child.Equals(parent))
-            {
-                childVertex.Parent = Find(parent);
-            }
-            return parent;
-        }
-
-        private void Union(T set1Representative, T set2Representative)
-        {
-            int set1RepresentativeIndex = indexLookup[set1Representative];
-            int set2RepresentativeIndex = indexLookup[set2Representative];
-            if(courses[set1RepresentativeIndex].Rank < courses[set2RepresentativeIndex].Rank)
-                courses[set1RepresentativeIndex].Parent = set2Representative;
-            else if (courses[set2RepresentativeIndex].Rank < courses[set1RepresentativeIndex].Rank)
-                courses[set2RepresentativeIndex].Parent = set1Representative;
-            else
-            {
-                courses[set1RepresentativeIndex].Parent = set2Representative;
-                courses[set2RepresentativeIndex].Rank++;
+                courses.AddEdge(dependencies[row, 1], dependencies[row, 0]);
             }
         }
 
         public bool IsPossibleToTakeAllCourses()
-        {
-            for (int row = 0; row < courseDependencies.GetLength(0); row++)
+        {            
+            T sourceVertex = courses.Vertices[0].Data;
+            HashSet<T> visited = new HashSet<T>();
+            HashSet<T> recursionStack = new HashSet<T>();
+            bool isCyclic = IsCyclic(sourceVertex, visited, recursionStack);
+            if(!isCyclic)
             {
-                if(!Find(courseDependencies[row, 0]).Equals(Find(courseDependencies[row, 1])))
+                for (int index = 0; index < courses.Vertices.Count; index++)
                 {
-                    Union(courseDependencies[row, 0], courseDependencies[row, 1]);
+                    if(!visited.Contains(courses.Vertices[index].Data))
+                    {
+                        if(IsCyclic(courses.Vertices[index].Data, visited, recursionStack))
+                        {
+                            isCyclic = true;
+                            break;
+                        }
+                    }
                 }
-                else
-                    return false;
             }
-            return true;
+
+            return !isCyclic;
+        }        
+
+        public bool IsCyclic(T vertex, HashSet<T> visited, HashSet<T> recursionStack)
+        {
+            if(recursionStack.Contains(vertex))
+                return true;
+            
+            if(visited.Contains(vertex))
+                return false;
+
+            visited.Add(vertex);
+            recursionStack.Add(vertex);
+            var neighbours = courses.GetNeighbours(vertex);         
+
+            foreach (var neighbour in neighbours)
+            {
+                if(IsCyclic(neighbour, visited, recursionStack))
+                    return true;
+            }
+
+            recursionStack.Remove(vertex);
+            return false;
         }
     }
 }
